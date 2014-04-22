a2eedce0d4dd051daf14e7e990097a440df47d01
Jesse van der Sar <jesse@sar.nl>
2014-04-22 19:12:54 +0200

    New version, will update readme asap (0.9.2)

--- odm.js
+++ odm.js
@@ -2,14 +2,37 @@ var uuid = require('node-uuid');
 
 module.exports = {};
 
-module.exports.createModel = function(name, fields, connection, cb) {
+module.exports.views_config_path = '';
+module.exports.views_config = {};
+
+/*module.exports.setViewsConfig = function(path) {
+  module.exports.views_config_path = path;
+
+  module.exports.views_config = require('../../'+ path);
+}*/
+
+module.exports.createScheme = function(name, fields, connection, cb) {
 
   var model = function Model(values) {
     if(!values) values = {};
     
     for(key in fields) {
       if(values[key] == undefined) {
-        values[key] = '';
+      	var type;
+
+        if(typeof(fields[key]) == 'object') {
+          type = fields[key].type;
+        } else {
+          type = fields[key];
+        }
+
+        if(type == String) {
+          values[key] = '';
+        } else if(type == Object) {
+          values[key] = {};
+        } else if(type == Array) {
+          values[key] = [];
+        }
       }
     }
     
@@ -29,7 +52,7 @@ module.exports.createModel = function(name, fields, connection, cb) {
   model.statics = {};
   
   model.validateField = function(field) {
-	var fieldTypes = [String, Number, Array, Date];
+	var fieldTypes = [String, Number, Array, Object, Date, Boolean];
   
     if(fieldTypes.indexOf(field) == -1) throw "Invalid field type: "+ field;
     return true;
@@ -53,22 +76,40 @@ module.exports.createModel = function(name, fields, connection, cb) {
 		}
 	  }
 	  
-	  if(typeof(key) == 'string') {
+	  if(typeof(key) == 'string' || typeof(key) == 'number') {
         q.key = key;
       } else if(typeof(key) == 'object') {
 	    q = key;
       }
       
+      var return_single = true;
+      if(q.return_single != undefined && q.return_single === false) {
+        return_single = false;
+      }
+      
       view.query(q, function(err, results) {
         if(err) throw err;
         
-        results = results.map(model.unserialize);
+        if(results[0] && results[0].value._id) {
+	        // Regular document
         
-        if(q.limit && q.limit == 1) {
-	        cb(results[0]);
-        } else {
-	        cb(results);
-        }
+	        results = results.map(model.unserialize);
+	        
+	        if(q.limit && q.limit == 1) {
+		        cb(results[0]);
+	        } else {
+		        cb(results);
+	        }
+	    } else {
+		    // Reduced result
+		    
+			if(results.length == 1 && return_single) {
+				cb(results[0].value);
+			} else {
+				cb(results);
+			}
+	    }
+	    
       })
     };
   };
@@ -93,7 +134,7 @@ module.exports.createModel = function(name, fields, connection, cb) {
   }
   
   // Get all
-  model.setViewFunc('all', 'getAll');
+  model.setViewFunc('all', 'all');
   
   model.prototype.save = function(cb) {
     var obj = this;
@@ -118,58 +159,72 @@ module.exports.createModel = function(name, fields, connection, cb) {
   };
   
   model.setupViews = function() {
+
 	  // Retrieve design document to import existing views
-	  connection.getDesignDoc(name, function(err, doc) {
+	  //connection.getDesignDoc(name, function(err, doc) {
+
+    var views = module.exports.views_config[name];
+
+    //connection.getDesignDoc(name, function(err, doc) {
 	    // Merge with Model views
 	    
-	    if(doc && doc.views) {
+	    /*if(doc && doc.views) {
 	      var views = doc.views;
 	    } else {
 	      var views = {};
-	    }
+	    }*/
 	
-		var indexViews = {};
+		  var indexViews = {};
 	    model.indexes.forEach(function(index) {
-	      var key = 'by_'+ index;
-	      indexViews[key] = { map: "function (doc, meta) {\n  if(doc._type == \'"+ name +"\') {\n    emit(doc."+ index +", doc);\n  }\n}" };
-	    });
-	    
-	    for(var viewName in views) {
-		    if(viewName != 'all') {
-			    if(!indexViews[viewName]) {
-				    
-				    indexViews[viewName] = views[viewName];
-					
-				}
-				
-			}
-	    };
-	    
-	    for(var viewName in indexViews) {
-		    var funcName = 'get';
-				
-			viewName.split('_').forEach(function(comp) {
-				funcName += comp.charAt(0).toUpperCase() + comp.slice(1);
-			});
-		
-			model.setViewFunc(viewName, funcName);
-	    }
-	    
-	    indexViews['all'] = { map: "function (doc, meta) {\n  if(doc._type == \'"+ name +"\') {\n    emit(null, doc);\n  }\n}" };
-	    
-	    connection.setDesignDoc(name, { views: indexViews }, function(err, results) {
-	      if(err) throw err;
+	        var key = 'by_'+ index;
+	        
+	        if(!views[key]) {
+	          indexViews[key] = { map: "function (doc, meta) {\n  if(doc._type == \'"+ name +"\') {\n    emit(doc."+ index +", doc);\n  }\n}" };
+	        }
+	      });
 	      
-	      //cb(model);
-	    });
-	  });
+	      for(var viewName in views) {
+
+          if(!indexViews[viewName]) {
+              views[viewName].map = views[viewName].map.toString();
+              if(views[viewName].reduce) {
+                views[viewName].reduce = views[viewName].reduce.toString();
+              }
+
+              indexViews[viewName] = views[viewName];
+          }
+
+	      };
+	      
+	      for(var viewName in indexViews) {
+	        var funcName = '';
+	        
+	        viewName.split('_').forEach(function(comp) {
+	          funcName += comp.charAt(0).toUpperCase() + comp.slice(1);
+	        });
+	        
+	        funcName = funcName.charAt(0).toLowerCase() + funcName.slice(1)
+	      
+	        model.setViewFunc(viewName, funcName);
+	      }
+	      
+	      if(!indexViews['all']) {
+	        indexViews['all'] = { map: "function (doc, meta) {\n  if(doc._type == \'"+ name +"\') {\n    emit(doc._id, doc);\n  }\n}" };
+	      }
+	    
+  	    connection.setDesignDoc(name, { views: indexViews }, function(err, results) {
+  	      if(err) throw err;
+  	      
+  	      //cb(model);
+  	    });
+	  //});
 	  
 	  return this;
   }
   
-  model.getById = function(id, cb, raw) {
+  model.byId = function(id, cb, raw) {
     connection.get(name +'_'+ id, function(err, result) {
-      if(err) throw err;
+      if(err) cb(false);
       
       if(raw) {
         cb(result);
